51eb809338d9559f119cfe782ece4981cb424d85
Arpan-sopho <mangalarpan@gmail.com>
2017-10-28 19:28:06 +0530

    removed duplicates

---
+++ Ass4/removeDuplicates.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+import java.util.ArrayList;
+
+public class removeDuplicates {
+    public static void main(String argsp[]) {
+    Scanner s = new Scanner(System.in);
+    int size = s.nextInt();
+    int newSize = 0;
+    String in1 = s.next();
+    System.out.println(in1);
+    newSize++;
+    String in2;
+    for (int i = 1; i < size; i++) {
+        in2 = s.next();
+        if (in1.equals(in2)){
+        } else {
+            System.out.println(in1);
+        }
+        newSize++;
+        in1 = in2;
+        
+    }
+    System.out.println(in1);
+    System.out.println(newSize);
+    }
+}
